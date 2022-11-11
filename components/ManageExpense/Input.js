6257efdb50e2c54da67ad7b88b95bb426b0d2054
Gaston <gaston.irby@gmail.com>
2022-11-11 10:58:24 -0800

    added a custom input component

---
+++ components/ManageExpense/Input.js
@@ -0,0 +1,10 @@
+import { View, Text, TextInput } from "react-native";
+
+function Input({ label, textInputConfig }) {
+    return <View>
+        <Text>{label}</Text>
+        <TextInput {...textInputConfig} />
+    </View>
+}
+
+export default Input;
