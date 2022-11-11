ee7bdfe18577521236277a9a14965d0fdba7bb46
Gaston <gaston.irby@gmail.com>
2022-11-11 09:19:23 -0800

    added context API using useReducer in a store folder

--- screens/ManageExpense.js
+++ screens/ManageExpense.js
@@ -15,11 +15,17 @@ function ManageExpense ({ route, navigation }) {
     })
   }, [navigation, isEditing])
 
-  function deleteExpenseHandler () {}
+  function deleteExpenseHandler () {
+    navigation.goBack();
+  }
 
-  function cancelHandler () {}
+  function cancelHandler () {
+    navigation.goBack();
+  }
 
-  function confirmHandler () {}
+  function confirmHandler () {
+    navigation.goBack();
+  }
 
   return (
     <View style={styles.container}>
