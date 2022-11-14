8a33111643ff9c493e209e19956d7636290d7509
Gaston <gaston.irby@gmail.com>
2022-11-13 16:35:02 -0800

    added loading spinner to update and delete as well

--- screens/ManageExpense.js
+++ screens/ManageExpense.js
@@ -1,13 +1,15 @@
-import { useContext, useLayoutEffect } from 'react'
+import { useContext, useLayoutEffect, useState } from 'react'
 import { View, StyleSheet } from 'react-native'
 
 import ExpenseForm from '../components/ManageExpense/ExpenseForm'
 import IconButton from '../components/UI/IconButton'
+import LoadingOverlay from '../components/UI/LoadingOverlay'
 import { GlobalStyles } from '../constants/styles'
 import { ExpensesContext } from '../store/expenses-context'
 import { storeExpense, updateExpense, deleteExpense } from '../util/http'
 
 function ManageExpense ({ route, navigation }) {
+  const [isSubmitting, setIsSubmitting] = useState();
   const expensesCtx = useContext(ExpensesContext)
 
   const editedExpenseId = route.params?.expenseId
@@ -24,6 +26,7 @@ function ManageExpense ({ route, navigation }) {
   }, [navigation, isEditing])
 
   async function deleteExpenseHandler () {
+    setIsSubmitting(true)
     await deleteExpense(editedExpenseId)
     expensesCtx.deleteExpense(editedExpenseId)
     navigation.goBack()
@@ -34,6 +37,7 @@ function ManageExpense ({ route, navigation }) {
   }
 
   async function confirmHandler (expenseData) {
+    setIsSubmitting(true)
     if (isEditing) {
       expensesCtx.updateExpense(editedExpenseId, expenseData)
       await updateExpense(editedExpenseId, expenseData);
@@ -42,6 +46,10 @@ function ManageExpense ({ route, navigation }) {
       expensesCtx.addExpense({...expenseData, id: id})
     }
     navigation.goBack()
+  }
+
+  if (isSubmitting) {
+    return <LoadingOverlay />;
   }
 
   return (
